352f3443a0d3c985fbfb539cd1496c88bb76d4e5
bunkerbuster <bunkerbusterdev@gmail.com>
2025-02-07 13:49:02 +0900

    연결 실패 예외처리리

--- src/app.ts
+++ src/app.ts
@@ -7,6 +7,9 @@ import AEService from 'services/AEService';
 import ThingService from 'services/ThingService';
 
 class App {
+    private state: string;
+    private parentHost: string;
+    private parentPort: number;
     private aeService: AEService;
     private thingService: ThingService;
     
@@ -19,45 +22,48 @@ class App {
         process.on('SIGINT', this.shutdown.bind(this));
         WatchdogTimer.startWatchdog();
 
-        this.aeService = new AEService(this.restart.bind(this));
-        this.thingService = new ThingService(this.aeService.sendToAE.bind(this.aeService));
+        this.state = 'connectThing';
+        this.parentHost = Config.thingAdaptionSoftware.parentHost;
+        this.parentPort = Config.thingAdaptionSoftware.parentPort;
+
+        this.aeService = new AEService(this.getState.bind(this), this.restart.bind(this));
+        this.thingService = new ThingService(this.getState.bind(this), this.aeService.sendToAE.bind(this.aeService));
 
         this.maxRetries = 5;
         this.retryCount = 0;
         this.delayTime = 1000;
-
-        Config.thingAdaptionSoftware.state = 'connectThing';
     }
 
     // 애플리케이션 시작
     public async start(): Promise<void> {
-        if(Config.thingAdaptionSoftware.state === 'connectThing') {
-            Config.thingAdaptionSoftware.state = await this.thingService.connect();
-        }
-        if(Config.thingAdaptionSoftware.state === 'connectAeClient' || Config.thingAdaptionSoftware.state === 'reconnectAeClient') {
-            Config.thingAdaptionSoftware.state = await this.aeService.connect();
-        }
-        if(Config.thingAdaptionSoftware.state === 'startThing') {
-            this.retryCount = 0;
-            this.delayTime = 1000;
-            Config.thingAdaptionSoftware.state = await this.thingService.startThing();
-        }
-        if(Config.thingAdaptionSoftware.state === 'startUpload') {
-            Logger.info('[App]: Thing Adaption Software is starting upload');
+        try {
+            if(this.state === 'connectThing') {
+                this.state = await this.thingService.setupSocket();
+            }
+            if(this.state === 'connectAeClient' || this.state === 'reconnectAeClient') {
+                this.state = await this.aeService.connect(this.parentHost, this.parentPort);
+            }
+            if(this.state === 'startThing') {
+                this.retryCount = 0;
+                this.delayTime = 1000;
+                this.state = await this.thingService.startThing();
+            }
+            if(this.state === 'startUpload') {
+                Logger.info('[App]: Thing Adaption Software is starting upload');
+            }
+        } catch (error) {
+            Logger.error(`[App-start]: App start is failed`);
+            this.restart();
         }
     }
 
     private async restart(): Promise<void> {
         Logger.info('[App-restart]: Restarting application...');
-        Config.thingAdaptionSoftware.state = 'reconnectAeClient';
+        this.state = 'reconnectAeClient';
+        await this.thingService.stopThing();
 
         // 현재 지연 시간만큼 대기
         this.retryCount++;
-        Logger.info(`[App-restart]: Retrying connection (${this.retryCount}/${this.maxRetries}) in ${this.delayTime}ms...`);
-        await Delay(this.delayTime);
-        
-        // 지수 백오프 계산 (최대 지연 시간 제한)
-        this.delayTime = Math.min(this.delayTime * 2, 30000); // 최대 지연 시간 30초
 
         // 최대 재시도 횟수 초과 시 에러 처리
         if (this.retryCount > this.maxRetries) {
@@ -65,11 +71,15 @@ class App {
             
             // 1분 후 재시작
             await Delay(60000); // 60초 대기
-            if (typeof this.restart === 'function') {
-                this.restart(); // App의 restart 메서드 호출
-            }
+            this.retryCount = 1;
+            this.delayTime = 1000;
         }
 
+        Logger.info(`[App-restart]: Retrying connection (${this.retryCount}/${this.maxRetries}) in ${this.delayTime}ms...`);
+        await Delay(this.delayTime);
+        
+        // 지수 백오프 계산 (최대 지연 시간 제한)
+        this.delayTime = Math.min(this.delayTime * 2, 30000); // 최대 지연 시간 30초
         await this.start();
     }
 
@@ -79,6 +89,10 @@ class App {
         WatchdogTimer.stopWatchdog();
         await this.aeService.disconnect();
         process.exit(0);
+    }
+
+    private getState(): string {
+        return this.state;
     }
 }
 
